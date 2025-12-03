4f87464d6bdb158aa726999bc4ac392e9c4f3ac7
Sauron <yakovmatvienko@gmail.com>
2025-12-03 09:19:49 +0200

    Day 3 Part 1

---
+++ 03.12.2025/part1/main.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <algorithm>
+
+/*
+--- Day 3: Lobby ---
+You descend a short staircase, enter the surprisingly vast lobby, and are quickly cleared by the security checkpoint. When you get to the main elevators, however, you discover that each one has a red light above it: they're all offline.
+
+"Sorry about that," an Elf apologizes as she tinkers with a nearby control panel. "Some kind of electrical surge seems to have fried them. I'll try to get them online soon."
+
+You explain your need to get further underground. "Well, you could at least take the escalator down to the printing department, not that you'd get much further than that without the elevators working. That is, you could if the escalator weren't also offline."
+
+"But, don't worry! It's not fried; it just needs power. Maybe you can get it running while I keep working on the elevators."
+
+There are batteries nearby that can supply emergency power to the escalator for just such an occasion. The batteries are each labeled with their joltage rating, a value from 1 to 9. You make a note of their joltage ratings (your puzzle input). For example:
+
+987654321111111
+811111111111119
+234234234234278
+818181911112111
+The batteries are arranged into banks; each line of digits in your input corresponds to a single bank of batteries. Within each bank, you need to turn on exactly two batteries; the joltage that the bank produces is equal to the number formed by the digits on the batteries you've turned on. For example, if you have a bank like 12345 and you turn on batteries 2 and 4, the bank would produce 24 jolts. (You cannot rearrange batteries.)
+
+You'll need to find the largest possible joltage each bank can produce. In the above example:
+
+In 987654321111111, you can make the largest joltage possible, 98, by turning on the first two batteries.
+In 811111111111119, you can make the largest joltage possible by turning on the batteries labeled 8 and 9, producing 89 jolts.
+In 234234234234278, you can make 78 by turning on the last two batteries (marked 7 and 8).
+In 818181911112111, the largest joltage you can produce is 92.
+The total output joltage is the sum of the maximum joltage from each bank, so in this example, the total output joltage is 98 + 89 + 78 + 92 = 357.
+
+There are many batteries in front of you. Find the maximum joltage possible from each bank; what is the total output joltage?
+*/
+
+using namespace std;
+
+using batteries = vector<string>;
+
+int ParseInput(const string& path, batteries& banks) {
+    ifstream file(path);
+    if (!file.good()) {
+        return -1;
+    }
+    string line;
+    while (getline(file, line)) {
+        banks.push_back(line);
+    }
+    return 0;
+}
+
+size_t MaxBatteryVoltage(const string& battery) {
+    string::const_iterator largestDigitPointer = max_element(battery.begin(), battery.end(), [](char a, char b) {
+        return a < b;
+    });
+    // if the largest digist found is the last, it wont work
+    if (largestDigitPointer == battery.end() - 1) {
+        largestDigitPointer = max_element(battery.begin(), battery.end() - 1, [](char a, char b) {
+            return a < b;
+        });
+    }
+    const auto& secondLargestDigit = max_element(largestDigitPointer + 1, battery.end(), [](char a, char b) {
+        return a < b;
+    });
+    char firstDigit = *largestDigitPointer;
+    char secondDigit = *secondLargestDigit;
+    string resultString{ firstDigit, secondDigit };
+
+    return stoi(resultString);
+}
+
+size_t CalculateMaxVoltage(const batteries& banks) {
+    size_t result = 0;
+    for (const auto& bank : banks) {
+        result += MaxBatteryVoltage(bank);
+    }
+    return result;
+}
+
+
+int main() {
+    batteries banks;
+    if (ParseInput("input.txt", banks)) {
+        return -1;
+    }
+    cout << CalculateMaxVoltage(banks) << "\n";
+    return 0;
+}
